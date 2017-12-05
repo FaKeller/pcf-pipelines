48766108aa4e3cb4dc52763b65d9d049e7ab97ac
Fabian Keller <login@fabian-keller.de>
2017-12-05 17:58:49 +0100

    Fix: order of abort method declaration

--- tasks-offline/upload-product-and-stemcell/task.sh
+++ tasks-offline/upload-product-and-stemcell/task.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+function abort() {
+  echo "${1}"
+  exit 1
+}
+
 if [[ -n "$NO_PROXY" ]]; then
   echo "$OM_IP $OPSMAN_DOMAIN_OR_IP_ADDRESS" >> /etc/hosts
 fi
@@ -79,8 +84,3 @@ fi
 # Should the slug contain more than one product, pick only the first.
 FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
 om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS -u $OPS_MGR_USR -p $OPS_MGR_PWD -k --request-timeout 3600 upload-product -p $FILE_PATH
-
-function abort() {
-  echo "${1}"
-  exit 1
-}
